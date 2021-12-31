7f23124de51727d8cc5df464c23624fd3e80d7d9
ANKE-SaiPrasad <89138270+ankesai8@users.noreply.github.com>
2021-12-31 12:59:54 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const PORT = process.env.PORT || 8008;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(logger("dev"));
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/exercises", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+console.log(process.env.MONGODB_URI);
+
+// routes
+app.use(require("./routes/apiroutes.js"));
+app.use(require("./routes/htmlroutes.js"));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}!`);
+});
