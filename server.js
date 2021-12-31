c201fd8be108055f54ebbaf43ce0f21f28907f8c
ANKE-SaiPrasad <89138270+ankesai8@users.noreply.github.com>
2021-12-31 13:06:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(logger("dev"));
 
-app.use(express.static("public"));
+app.use(express.static("frontend"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/exercises", {
   useNewUrlParser: true,
@@ -24,8 +24,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/exercises", {
 console.log(process.env.MONGODB_URI);
 
 // routes
-app.use(require("./routes/apiroutes.js"));
-app.use(require("./routes/htmlroutes.js"));
+app.use(require("./Router/apiroutes.js"));
+app.use(require("./Router/htmlroutes.js"));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}!`);
